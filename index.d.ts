dde658650e1bf74f77a20cbc6d89f764459db39c
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-13 18:25:51 -0300

    fix debug

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import * as mongoose from 'mongoose';
 import { Repository } from 'graphql-api-scripts';
 
-export function MongooseRepository (folder: string, options: mongoose.ConnectionOptions, debug?: false): Repository
+export function MongooseRepository (folder: string, options: mongoose.ConnectionOptions, debug?: boolean): Repository
